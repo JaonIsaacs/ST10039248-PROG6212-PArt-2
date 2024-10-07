78031a80c8a95f819b13192c6cb56f0ef6cfb883
JaonIsaacs <102227507+JaonIsaacs@users.noreply.github.com>
2024-10-07 13:30:32 +0200

    Added claim class

---
+++ Claim.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST10039248_PROG6212
+{
+    public class Claim
+    {
+        public int ClaimId { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+        public List<string> Documents { get; set; }
+    }
+}
