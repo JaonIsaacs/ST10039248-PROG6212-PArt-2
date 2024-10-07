b6e1b5dd6d651c7fee83dc5ce47a7055017a5860
JaonIsaacs <102227507+JaonIsaacs@users.noreply.github.com>
2024-10-07 13:02:56 +0200

    Add project files.

---
+++ SubmitClaimPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ST10039248_PROG6212
+{
+    /// <summary>
+    /// Interaction logic for SubmitClaimPage.xaml
+    /// </summary>
+    public partial class SubmitClaimPage : Page
+    {
+        public SubmitClaimPage()
+        {
+            InitializeComponent();
+        }
+        private void AttachDocument_Click(object sender, RoutedEventArgs e)
+        {
+            // Logic to attach a document (use OpenFileDialog)
+        }
+
+        private void SubmitClaim_Click(object sender, RoutedEventArgs e)
+        {
+            // Logic to submit the claim and save it
+        }
+    }
+}
