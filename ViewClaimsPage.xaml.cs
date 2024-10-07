83a340a7e7a05531ba2fc993863a2eadbcf6a08f
JaonIsaacs <102227507+JaonIsaacs@users.noreply.github.com>
2024-10-07 13:21:48 +0200

    added ViewClaimsPage

---
+++ ViewClaimsPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ST10039248_PROG6212
+{
+    /// <summary>
+    /// Interaction logic for ViewClaimsPage.xaml
+    /// </summary>
+    public partial class ViewClaimsPage : Page
+    {
+        public ViewClaimsPage()
+        {
+            InitializeComponent();
+            LoadClaims();
+        }
+
+        private void LoadClaims()
+        {
+            // Logic to load and display claims in ClaimsList
+        }
+    }
+}
